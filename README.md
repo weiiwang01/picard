82864b970ad03e8500f51693a4ed7a0cbde1797d
Weii Wang <weii.wang@canonical.com>
2024-10-06 17:16:23 +0000

    PiCard v2

---
+++ README.md
@@ -0,0 +1,6 @@
+# PiCard
+
+> [!IMPORTANT]  
+> PiCard is currently under development.
+
+PiCard is a miniaturized Raspberry Pi Compute Module carrier board optimized for Linux server use cases.
---
+++ convert_bom.py
@@ -0,0 +1,26 @@
+import csv
+import pathlib
+import sys
+
+production = pathlib.Path(__file__).parent / "production"
+
+positions = csv.reader((production / "positions.csv").open())
+connectors = []
+
+for row in positions:
+    reference, x, y, rotation, layer = row
+    if not reference.startswith("ComputeModule"):
+        continue
+    x = float(x)
+    y = float(y)
+    connectors.append((f"X{len(connectors) + 1}", x + 21.5, y + 0.46, 90, layer))
+    connectors.append((f"X{len(connectors) + 1}", x + 21.5, y - 33.46, 90, layer))
+
+(production / "positions.csv").open(mode="a").write(
+    "\n".join(",".join(map(str, c)) for c in connectors) + "\n"
+)
+(production / "bom.csv").open(mode="a").write(
+    '"{}",10164227-1001A1RLF,{},10164227-1001A1RLF,C6782225'.format(
+        ", ".join(r[0] for r in connectors), len(connectors)
+    )
+)
